9a4a6923b3d6cdf3626cc93cf33e76351fa7ec00
Giovanny Otarola <giovannyxdd@gmail.com>
2024-06-07 13:41:59 -0400

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { IndexComponent } from './components/index/index.component';
+import { RecreativoComponent } from './components/recreativo/recreativo.component';
+import { FamiliaComponent } from './components/familia/familia.component';
+import { AventuraComponent } from './components/aventura/aventura.component';
+import { RolComponent } from './components/rol/rol.component';
 
 export const routes: Routes = [
     //esto indica cual es la primera pagina que se debe inicar junto con la app
-    {path: '**', redirectTo: 'login'},
+    {path: '**', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent},
+    {path: 'index', component: IndexComponent},
+    {path: 'recreativo',component: RecreativoComponent},
+    {path: 'familia', component: FamiliaComponent},
+    {path: 'aventura',component: AventuraComponent},
+    {path: 'rol', component: RolComponent}
 ];
