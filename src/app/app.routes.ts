04aab7c3432272aa2863bdfdc0159114abd8c005
Giovanny Otarola <giovannyxdd@gmail.com>
2024-06-10 17:19:16 -0400

    nav filtrando bien

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { IndexComponent } from './components/index/index.component';
 import { PrincipalComponent } from './components/principal/principal.component';
-
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 export const routes: Routes = [
     //esto indica cual es la primera pagina que se debe inicar junto con la app
@@ -11,6 +12,11 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
     {path: 'index', component: IndexComponent},
-    {path: 'principal', component: PrincipalComponent}
-    
+    {path: 'principal', component: PrincipalComponent}, 
+    { path: 'principal/:categoria', component: PrincipalComponent } // Ruta dinámica que recibe la categoría como parámetro
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Renderer2, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+interface Juego {
+  categoria: string;
+  nombre: string;
+  descripcion: string;
+  precio: String; 
+  descuento: String; 
+  imagen: string;
+}
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.scss'
+  styleUrls: ['./principal.component.scss']
 })
-export class PrincipalComponent {
+export class PrincipalComponent  {
+  
+  juegos: Juego[] = [
+    //aventura
+    {categoria:'aventura', nombre:'aventura 1', descripcion: 'Descripcion juego 1', precio:'$ 50', descuento:'no', imagen:'assets/images/aventura1.jpg'},
+    {categoria:'aventura', nombre:'aventura 2', descripcion: 'Descripcion juego 2', precio:'$ 50', descuento:'no', imagen:'assets/images/aventura2.jpg'},
+    {categoria:'aventura', nombre:'aventura 3', descripcion: 'Descripcion juego 3', precio:'$ 50', descuento:'no', imagen:'assets/images/aventura3.png'},
+    {categoria:'aventura', nombre:'aventura 4', descripcion: 'Descripcion juego 4', precio:'$ 50', descuento:'no', imagen:'assets/images/aventura4.jpg'},
+    //familia
+    {categoria:'familia', nombre:'familia 1', descripcion: 'Descripcion juego 1', precio:'$ 50', descuento:'no', imagen:'assets/images/familia1.jpg'},
+    {categoria:'familia',nombre:'familia 2', descripcion: 'Descripcion juego 2', precio:'$ 50', descuento:'no', imagen:'assets/images/familia2.jpg'},
+    {categoria:'familia',nombre:'familia 3', descripcion: 'Descripcion juego 3', precio:'$ 50', descuento:'no', imagen:'assets/images/familia3.jpg'},
+     
+    //Recreativo
+    {categoria:'recreativo', nombre:'Recreativo 1', descripcion: 'Descripcion juego 1', precio:'$ 50', descuento:'no', imagen:'assets/images/recreativo1.jpg'},
+    {categoria:'recreativo',nombre:'Recreativo 2', descripcion: 'Descripcion juego 2', precio:'$ 50', descuento:'no', imagen:'assets/images/recreativo2.jpg'},
+    {categoria:'recreativo',nombre:'Recreativo 3', descripcion: 'Descripcion juego 3', precio:'$ 50', descuento:'no', imagen:'assets/images/recreativo3.jpg'},
+      
+      //Rol
+      {categoria:'rol', nombre:'Rol 1', descripcion: 'Descripcion juego 1', precio:'$ 50', descuento:'no', imagen:'assets/images/rol1.jpg'},
+      {categoria:'rol',nombre:'Rol 2', descripcion: 'Descripcion juego 2', precio:'$ 50', descuento:'no', imagen:'assets/images/rol2.jpg'},
+      {categoria:'rol',nombre:'Rol 3', descripcion: 'Descripcion juego 3', precio:'$ 50', descuento:'no', imagen:'assets/images/rol3.jpg'},
+  
+  ];
 
+  juegosFiltrados: Juego[] = [];
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // aquí puedes cargar los juegos iniciales o manejar la lógica de filtrado si se proporciona una categoría en la ruta
+    this.route.params.subscribe(params => {
+      const categoria = params['categoria'];
+      if (categoria) {
+        this.filtrarPorCategoria(categoria);
+      } else {
+        // Si no se proporciona una categoría, mostrar todos los juegos
+        this.juegosFiltrados = this.juegos;
+      }
+    });
+  }
+
+  filtrarPorCategoria(categoria: string) {
+    this.juegosFiltrados = this.juegos.filter(juego => juego.categoria === categoria);
+  }
+  
 }
